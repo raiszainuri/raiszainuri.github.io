a8adcfdf4777069c3c381ceccfd8c90199a5d8aa
Rais Zainuri <raiszainuri09@gmail.com>
2024-11-22 17:44:48 +0700

    add: commit for md32

---
+++ 32/script.js
@@ -0,0 +1,280 @@
+const alumniData = [
+    {
+        "nickname": "Adrian",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Ahmad",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Akbar",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Alvin",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Candra",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Dewi",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Eko",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Falih",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Fisan",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Tanto",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Melda",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Anin",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Hasfi",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Aril",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Ardi",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Jalil",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Rizki",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Nara",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Danang",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Raja",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Reza",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Ryan",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Tika",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Abel",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    },
+    {
+        "nickname": "Yunus",
+        "campus": "Nama Universitas",
+        "ig": "",
+        "linkedin": "",
+        "photo": "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-875.jpg"
+    }
+];
+
+const randomImages = [
+    "https://picsum.photos/200/300?random=1",
+    "https://picsum.photos/200/300?random=2",
+    "https://picsum.photos/200/300?random=3",
+    "https://picsum.photos/200/300?random=4",
+    "https://picsum.photos/200/300?random=5",
+    "https://picsum.photos/200/300?random=6",
+    "https://picsum.photos/200/300?random=7",
+    "https://picsum.photos/200/300?random=8",
+    "https://picsum.photos/200/300?random=9",
+    "https://picsum.photos/200/300?random=10",
+    "https://picsum.photos/200/300?random=11",
+    "https://picsum.photos/200/300?random=12",
+    "https://picsum.photos/200/300?random=13",
+    "https://picsum.photos/200/300?random=14",
+    "https://picsum.photos/200/300?random=15",
+    "https://picsum.photos/200/300?random=16",
+    "https://picsum.photos/200/300?random=17",
+    "https://picsum.photos/200/300?random=18",
+    "https://picsum.photos/200/300?random=19",
+    "https://picsum.photos/200/300?random=20",
+    "https://picsum.photos/200/300?random=21",
+    "https://picsum.photos/200/300?random=22",
+    "https://picsum.photos/200/300?random=23",
+    "https://picsum.photos/200/300?random=24",
+    "https://picsum.photos/200/300?random=25"
+];
+
+
+function createAlumniCard(alumni) {
+    const col = document.createElement('div');
+    col.className = 'col-md-4 col-lg-3';
+
+    const card = document.createElement('div');
+    card.className = 'alumni-card';
+
+    const img = document.createElement('img');
+    if (alumni.photo === null) {img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/340px-Default_pfp.svg.png";}
+    else {img.src = alumni.photo;}
+    img.alt = `${alumni.nickname} photo`;
+    
+    const name = document.createElement('h2');
+    name.textContent = alumni.nickname;
+    
+    const campus = document.createElement('p');
+    campus.textContent = alumni.campus;
+    
+    const links = document.createElement('div');
+    links.className = 'links';
+    
+    const igLink = document.createElement('a');
+    igLink.href = alumni.ig;
+    igLink.textContent = 'Instagram';
+    igLink.target = '_blank';
+    
+    const separator = document.createElement('span');
+    separator.textContent = '  ';
+
+    const linkedinLink = document.createElement('a');
+    linkedinLink.href = alumni.linkedin;
+    linkedinLink.textContent = 'LinkedIn';
+    linkedinLink.target = '_blank';
+
+    links.appendChild(igLink);
+    links.appendChild(separator);
+    links.appendChild(linkedinLink);
+    
+    card.appendChild(img);
+    card.appendChild(name);
+    card.appendChild(campus);
+    card.appendChild(links);
+    
+    col.appendChild(card);
+    return col;
+}
+
+function assignRandomImages(alumniData, images) {
+    return alumniData.map(alumni => {
+        return {
+            ...alumni,
+            photo: images[Math.floor(Math.random() * images.length)]
+        };
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const alumniContainer = document.getElementById('alumniContainer');
+
+    const alumniWithRandomImages = assignRandomImages(alumniData, randomImages);
+    const shuffledAlumniData = alumniWithRandomImages.sort(() => Math.random() - 0.5);
+
+    shuffledAlumniData.forEach(alumni => {
+        const col = createAlumniCard(alumni);
+        alumniContainer.appendChild(col);
+    });
+
+    new Masonry('#alumniContainer', {
+        itemSelector: '.col-md-4',
+        columnWidth: '.col-md-4',
+        percentPosition: true
+    });
+});
